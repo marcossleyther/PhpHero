6cceaac93f08ee8c3e2299f697954054663d2c88
Sleyther <marcossleyther@gmail.com>
2020-12-14 22:26:27 -0600

    Los Seeder

---
+++ hero/database/seeders/LevelsTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class LevelsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $xp = 100;
+
+        for ($i = 0; $i < 10; $i++) {
+
+            $xp = $xp * 2;
+            DB::table('levels')->insert([
+                'xp' => $xp
+            ]);
+
+        }
+    }
+}
