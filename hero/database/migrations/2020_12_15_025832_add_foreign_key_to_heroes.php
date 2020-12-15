67afb02b7b52a9e4cbe948bf68dc46c2555ecad8
Sleyther <marcossleyther@gmail.com>
2020-12-14 21:34:32 -0600

    Se argregan los modelos

---
+++ hero/database/migrations/2020_12_15_025832_add_foreign_key_to_heroes.php
@@ -0,0 +1,34 @@
+<?php
+
+use Brick\Math\BigInteger;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignKeyToHeroes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('heroes', function (Blueprint $table) {
+            $table->biginteger('level_Id')->unsigned();
+            $table->foreign('level_Id')->references('Id')->on('levels');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('heroes', function (Blueprint $table) {
+            //
+        });
+    }
+}
